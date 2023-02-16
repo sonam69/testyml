4d04b352f7cfab7c11cebf154ec981d2a47a4e68
Manos Tzelepis <tzeldev@gmail.com>
2023-02-16 02:13:43 +0200

    fix

--- dangerfile.js
+++ dangerfile.js
@@ -2,14 +2,14 @@ import {danger, fail, message} from "danger"
 
 const achievment = "Achievment unlocked rocket ";
 
-const { additions } = danger.github.pir;
-const { deletions } = danger.github.pir;
-if (additions < deletions) {
-    message(`${achievment} More code deleted than added!`);
-}
-else {
-    message(`${achievment} More code added than deleted pff!`);
-}
+// const { additions } = danger.github.pir;
+// const { deletions } = danger.github.pir;
+// if (additions < deletions) {
+//     message(`${achievment} More code deleted than added!`);
+// }
+// else {
+//     message(`${achievment} More code added than deleted pff!`);
+// }
 
 const pngBools = danger.git.fileMatch("**/*.png");
 const jpgBools = danger.git.fileMatch("**/*.jpg");
